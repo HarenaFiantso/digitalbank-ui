5b4cc6282ceb135faa7726f4a0334bed78696b41
HarenaFiantso <fiantsorav@gmail.com>
2024-03-14 08:11:15 +0300

    chore: poppins as font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,49 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
+import localFont from 'next/font/local'
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = localFont({
+  src: [
+    {
+      path: '../../public/fonts/Poppins-Thin.ttf',
+      weight: '100'
+    },
+    {
+      path: '../../public/fonts/Poppins-ExtraLight.ttf',
+      weight: '200'
+    },
+    {
+      path: '../../public/fonts/Poppins-Light.ttf',
+      weight: '300'
+    },
+    {
+      path: '../../public/fonts/Poppins-Regular.ttf',
+      weight: '400'
+    },
+    {
+      path: '../../public/fonts/Poppins-Medium.ttf',
+      weight: '500'
+    },
+    {
+      path: '../../public/fonts/Poppins-SemiBold.ttf',
+      weight: '600'
+    },
+    {
+      path: '../../public/fonts/Poppins-Bold.ttf',
+      weight: '700'
+    },
+    {
+      path: '../../public/fonts/Poppins-ExtraBold.ttf',
+      weight: '800'
+    },
+    {
+      path: '../../public/fonts/Poppins-Black.ttf',
+      weight: '900'
+    }
+  ],
+  variable: '--font-poppins'
+})
 
 export const metadata: Metadata = {
   title: "Digital bank",
@@ -16,8 +56,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={`${poppins.variable} font-sans`}>
+      <body>{children}</body>
     </html>
   );
 }
