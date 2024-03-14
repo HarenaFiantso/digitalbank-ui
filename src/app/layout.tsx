509b3ab17988678aed563a239a1173391c481e98
HarenaFiantso <fiantsorav@gmail.com>
2024-03-14 08:24:48 +0300

    chore: config prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,53 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import React from "react";
-import localFont from 'next/font/local'
+import './globals.css';
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import React from 'react';
 
 const poppins = localFont({
   src: [
     {
       path: '../../public/fonts/Poppins-Thin.ttf',
-      weight: '100'
+      weight: '100',
     },
     {
       path: '../../public/fonts/Poppins-ExtraLight.ttf',
-      weight: '200'
+      weight: '200',
     },
     {
       path: '../../public/fonts/Poppins-Light.ttf',
-      weight: '300'
+      weight: '300',
     },
     {
       path: '../../public/fonts/Poppins-Regular.ttf',
-      weight: '400'
+      weight: '400',
     },
     {
       path: '../../public/fonts/Poppins-Medium.ttf',
-      weight: '500'
+      weight: '500',
     },
     {
       path: '../../public/fonts/Poppins-SemiBold.ttf',
-      weight: '600'
+      weight: '600',
     },
     {
       path: '../../public/fonts/Poppins-Bold.ttf',
-      weight: '700'
+      weight: '700',
     },
     {
       path: '../../public/fonts/Poppins-ExtraBold.ttf',
-      weight: '800'
+      weight: '800',
     },
     {
       path: '../../public/fonts/Poppins-Black.ttf',
-      weight: '900'
-    }
+      weight: '900',
+    },
   ],
-  variable: '--font-poppins'
-})
+  variable: '--font-poppins',
+});
 
 export const metadata: Metadata = {
-  title: "Digital bank",
-  description: "Web digital banking application",
+  title: 'Digital bank',
+  description: 'Web digital banking application',
 };
 
 export default function RootLayout({
@@ -56,7 +56,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${poppins.variable} font-sans`}>
+    <html lang='en' className={`${poppins.variable} font-sans`}>
       <body>{children}</body>
     </html>
   );
