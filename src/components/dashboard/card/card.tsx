a7a8d851f0e4827e40b9860eab815487c8b2d7b1
Fiantso Harena <fiantsorav@gmail.com>
2024-03-17 07:55:47 +0300

    style: card✅

--- src/components/dashboard/card/card.tsx
+++ src/components/dashboard/card/card.tsx
@@ -2,15 +2,15 @@ import { MdSupervisedUserCircle } from 'react-icons/md';
 
 export default function Card() {
   return (
-    <div className="container">
-      <MdSupervisedUserCircle size={24} />
-      <div className="text">
-        <span className="title">Title</span>
-        <span className="number">Number</span>
-        <span className="details">
-          <span className="">
+    <div className="bg-main-soft h-max p-5 rounded-xl flex gap-5 cursor-pointer w-max hover:bg-hover transition-all">
+      <MdSupervisedUserCircle size={24} className="text-blue" />
+      <div className="flex flex-col gap-2">
+        <span className="font-semibold text-blue mb-2">Total accounts</span>
+        <span className="text-2xl">69</span>
+        <span className="text-sm text-color">
+          <span className="text-lime-500">
             69%
-          </span>{' '}
+          </span>{" "}
           More than previous week
         </span>
       </div>
