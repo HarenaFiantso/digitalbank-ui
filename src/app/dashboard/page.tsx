026eb4294bfb7180f1becf52e127f9fa994463cd
Fiantso Harena <fiantsorav@gmail.com>
2024-03-18 14:48:33 +0300

    feat: transaction overview component initialized

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,37 +1,37 @@
-import { BiDownload } from 'react-icons/bi';
-import Image from 'next/image';
 import { Money } from '../../../public/assets';
 import Card from '@/components/dashboard/card/card';
+import Transactions from '@/components/dashboard/transactions/transactions';
 import { cards } from '@/data/data';
 import { CardItem } from '@/types/Card';
+import Image from 'next/image';
+import { BiDownload } from 'react-icons/bi';
 
 export default function Dashboard() {
   return (
     <div className='mt-5'>
       <div className='flex flex-wrap justify-between'>
-        <div
-          className="bg-main-soft rounded-xl w-max p-8 pt-9">
-          <div className="flex gap-5 justify-between items-center">
+        <div className='w-max rounded-xl bg-main-soft p-8 pt-9'>
+          <div className='flex items-center justify-between gap-5'>
             <div>
-              <p className="font-bold text-blue">Welcome back,</p>
-              <p className="text-2xl mb-5">Fiantso Harena</p>
-              <p className="text-sm text-light">Download your latest statement</p>
+              <p className='font-bold text-blue'>Welcome back,</p>
+              <p className='mb-5 text-2xl'>Fiantso Harena</p>
+              <p className='text-sm text-light'>Download your latest statement</p>
             </div>
-            <Image src={Money} width={100} height={100} alt="money" />
+            <Image src={Money} width={100} height={100} alt='money' />
           </div>
-          <div className="mt-6">
-            <button className="bg-blue w-full rounded hover:bg-blueSoft transition-all flex items-center justify-center gap-2 p-2">
+          <div className='mt-6'>
+            <button className='flex w-full items-center justify-center gap-2 rounded bg-blue p-2 transition-all hover:bg-blueSoft'>
               <BiDownload size={20} /> Download
             </button>
           </div>
         </div>
-
-        <div className='flex gap-5 justify-between'>
+        <div className='flex justify-between gap-5'>
           {cards.map((item: CardItem) => (
             <Card item={item} key={item.id} />
           ))}
         </div>
       </div>
+      <Transactions />
     </div>
   );
 }
--- src/data/data.tsx
+++ src/data/data.tsx
@@ -20,7 +20,7 @@ export const data = [
         icon: <MdDashboard />,
       },
       {
-        title: 'Transaction',
+        title: 'Transactions',
         path: '/dashboard/transaction-list',
         icon: <MdFormatListBulleted />,
       },
@@ -76,20 +76,20 @@ export const data = [
 export const cards = [
   {
     id: 1,
-    title: "Total accounts",
-    number: "69",
+    title: 'Total accounts',
+    number: '69',
     change: 2,
   },
   {
     id: 2,
-    title: "Current balance",
-    number: "69.225",
+    title: 'Current balance',
+    number: '69.225',
     change: -2,
   },
   {
     id: 3,
-    title: "Total debt",
-    number: "6.642",
+    title: 'Total debt',
+    number: '6.642',
     change: 18,
   },
 ];
