e1b6f3428bef56a1208df437730542f4a0cf8925
Fiantso Harena <fiantsorav@gmail.com>
2024-03-18 20:44:58 +0300

    chore: add logic to latest transactions

--- src/components/dashboard/transactions/transactions.tsx
+++ src/components/dashboard/transactions/transactions.tsx
@@ -1,35 +1,43 @@
 import { NoAvatar } from '../../../../public/assets';
 import Image from 'next/image';
+import { transactions } from '@/data/data';
 
 export default function Transactions() {
   return (
-    <div className='bg-main-soft p-5 rounded-xl mt-5'>
-      <h2 className='mb-5 text-xl font-semibold text-blue'>Latest Transactions</h2>
-      <table className='w-full'>
+    <div className="bg-main-soft p-5 rounded-xl mt-5">
+      <h2 className="mb-5 text-xl font-semibold text-blue">Latest Transactions 🚀</h2>
+      <table className="w-full">
         <thead>
         <tr className="mb-2">
-          <td className='p-2 text-color'>Name</td>
-          <td className='p-2 text-color'>Status</td>
-          <td className='p-2 text-color'>Date</td>
-          <td className='p-2 text-color'>Amount</td>
+          <td className="p-2 text-color">Name</td>
+          <td className="p-2 text-color">Reason</td>
+          <td className="p-2 text-color">Type</td>
+          <td className="p-2 text-color">Date</td>
+          <td className="p-2 text-color">Amount</td>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>
-            <div className='flex gap-2 items-center'>
-              <Image src={NoAvatar} width={40} height={40} alt='avatar' className="object-cover rounded-full" />
-              Harena Fiantso
-            </div>
-          </td>
-          <td>
-              <span className="rounded-md p-2 text-sm font-bold bg-[#f7cb7375]">
-                Pending
+        {transactions.map(transaction => (
+          <>
+            <tr key={transaction.id}>
+              <td>
+                <div className="flex gap-2 items-center">
+                  <Image src={NoAvatar} width={40} height={40} alt="avatar" className="object-cover rounded-full" />
+                  {transaction.name}
+                </div>
+              </td>
+              <td>{transaction.reason.slice(0, 30)}</td>
+              <td>
+              <span className={`rounded p-2 text-sm font-bold text-center ${transaction.type === 'Credit' ? 'bg-lime-800' : 'bg-[#f7737375]'}`}>
+                {transaction.type}
               </span>
-          </td>
-          <td>14.02.2024</td>
-          <td>38.000Ar</td>
-        </tr>
+              </td>
+              <td>{transaction.date}</td>
+              <td>{transaction.amount}</td>
+            </tr>
+            <br />
+          </>
+        ))}
         </tbody>
       </table>
     </div>
--- src/data/data.tsx
+++ src/data/data.tsx
@@ -93,3 +93,30 @@ export const cards = [
     change: 18,
   },
 ];
+
+export const transactions = [
+  {
+    id: 1,
+    name: 'Fiantso',
+    reason: 'troubleshooting',
+    type: 'Credit',
+    date: '18.03.2024',
+    amount: '38.000'
+  },
+  {
+    id: 2,
+    name: 'Tanjona',
+    reason: 'Smatchin ticket',
+    type: 'Debit',
+    date: '18.03.2024',
+    amount: '3.000'
+  },
+  {
+    id: 3,
+    name: "Tendry",
+    reason: 'Alcohol',
+    type: 'Credit',
+    date: '18.03.2024',
+    amount: '50.000'
+  }
+]
