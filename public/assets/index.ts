141b30b736b3b5443c88d4e3be08bddcb52a9977
Fiantso Harena <fiantsorav@gmail.com>
2024-03-22 18:21:56 +0300

    style: add delete account UI

--- public/assets/index.ts
+++ public/assets/index.ts
@@ -1,6 +1,7 @@
 import Money from './money.webp';
 import NoAvatar from './noavatar.png';
 import AddAccount from './addAccount.png'
-import CreateAccount from './createAccount.png'
+import CreateAccount from './createAccount.png';
+import AccountNotFount from './accountNotFound.webp';
 
-export { Money, NoAvatar, AddAccount, CreateAccount };
+export { Money, NoAvatar, AddAccount, CreateAccount, AccountNotFount };
--- src/app/(auth)/chooseAccount/page.tsx
+++ src/app/(auth)/chooseAccount/page.tsx
@@ -1,13 +1,14 @@
-'use client'
+'use client';
 
 import Image from 'next/image';
-import { AddAccount, NoAvatar } from '../../../../public/assets';
+import { AccountNotFount, AddAccount, NoAvatar } from '../../../../public/assets';
 import { useEffect, useState } from 'react';
 import { fetchAccounts } from '@/api/Accounts';
 import Link from 'next/link';
+import { BiTrash } from 'react-icons/bi';
 
 export default function ChooseAccount() {
-  const [accounts, setAccounts] = useState<Account[]>([])
+  const [accounts, setAccounts] = useState<Account[]>([]);
   useEffect(() => {
     fetchAccounts().then(setAccounts);
   }, []);
@@ -26,16 +27,33 @@ export default function ChooseAccount() {
           <h1 className="text-sm font-semibold mb-6 text-gray-500 text-center">Choose your account to benefits with our
             platform</h1>
           <div className="bg-main-soft px-5 py-2 shadow-2xl rounded-2xl">
-            {accounts.map((account: Account) => (
-              <div key={account.idAccount}
-                   className="flex gap-5 border-b-gray-200 items-center my-6 px-5 py-2 cursor-pointer rounded-xl hover:bg-hover transition-all">
-                <Image src={NoAvatar} alt="avatar" width={50} height={50} className="rounded-full object-cover" />
-                <h1>{account.firstName} {account.lastName}</h1>
+            {accounts.length === 0 ? (
+              <div className='flex flex-col gap-5 items-center'>
+                <Image src={AccountNotFount} alt="account not found" width={100} height={100} />
+                <h1>No account found</h1>
               </div>
-            ))}
+            ) : (
+              accounts.map((account: Account) => (
+                <div
+                  key={account.idAccount}
+                  className="flex justify-between items-center my-6 px-5 py-2 cursor-pointer rounded-xl hover:bg-hover transition-all"
+                >
+                  <div className='flex gap-5 items-center'>
+                    <Image src={NoAvatar} alt="avatar" width={50} height={50} className="rounded-full object-cover" />
+                    <h1>
+                      {account.firstName} {account.lastName}
+                    </h1>
+                  </div>
+                  <div className='bg-hover p-3 rounded-full hover:bg-gray-800'>
+                    <BiTrash size={20} className='text-red-500' />
+                  </div>
+                </div>
+              ))
+            )}
           </div>
           <div className="mt-10 text-sm text-gray-500 text-center">
-            <p>Not found you account here? <Link href={"/addAccount"} className="text-blue hover:underline">Create one</Link>
+            <p>Not found you account here? <Link href={'/addAccount'} className="text-blue hover:underline">Create
+              one</Link>
             </p>
           </div>
         </div>
