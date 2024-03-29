8dcef5b7ec15a579c6a6ff8af302efd8363dbe63
HarenaFiantso <fiantsorav@gmail.com>
2024-03-29 19:29:08 +0300

    fix: navbar page title

--- src/components/dashboard/navbar/navbar.tsx
+++ src/components/dashboard/navbar/navbar.tsx
@@ -6,11 +6,11 @@ import { ToastContainer, toast } from 'react-toastify';
 
 export default function Navbar() {
   const pathname: string[] = usePathname().split('/');
-  const pageTitle: string = pathname[1];
+  const pageTitle: string = pathname[2];
 
   return (
     <div className='flex items-center justify-between rounded bg-main-soft p-5'>
-      <div className='text-xl font-bold capitalize text-blue'>{pageTitle}</div>
+      <div className='text-xl font-bold capitalize text-blue'>{pageTitle || "Dashboard"}</div>
       <div className='flex items-center gap-5'>
         <div className='flex justify-between gap-5'>
           <button onClick={() => toast('Soon !')}>
