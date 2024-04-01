b2eb243de7a51fcdc3655c1160dfc344deb2f396
HarenaFiantso <fiantsorav@gmail.com>
2024-04-01 19:35:40 +0300

    chore: transfer form initialized

--- src/app/dashboard/transfer/page.tsx
+++ src/app/dashboard/transfer/page.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link';
+import React from 'react';
+
 export default function TransferList() {
-  return <>Transfer List</>;
+  return (
+    <div className='relative overflow-x-auto'>
+      <h2 className='my-5 text-xl font-semibold text-blue'>All transfers made</h2>
+      <p className='mb-10 text-sm text-light'>
+        Would you like make a transfer?{' '}
+        <Link href={'/dashboard/transfer/makeTransfer'} className='hover:underline'>
+          Click here
+        </Link>
+      </p>
+    </div>
+  );
 }
