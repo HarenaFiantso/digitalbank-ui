fc7de7407cd3359f77a5b653c2df94473e3cb1a4
HarenaFiantso <fiantsorav@gmail.com>
2024-04-02 07:18:06 +0300

    chore: finish the implementation of the transfer feature

--- src/app/dashboard/transfer/page.tsx
+++ src/app/dashboard/transfer/page.tsx
@@ -1,16 +1,18 @@
-import Link from 'next/link';
-import React from 'react';
+import { Transfer } from '../../../../public/assets';
+import TransferForm from '@/components/dashboard/transfers/transferForm';
+import Image from 'next/image';
 
-export default function TransferList() {
+export default function NewTransfer() {
   return (
-    <div className='relative overflow-x-auto'>
-      <h2 className='my-5 text-xl font-semibold text-blue'>All transfers made</h2>
-      <p className='mb-10 text-sm text-light'>
-        Would you like make a transfer?{' '}
-        <Link href={'/dashboard/transfer/makeTransfer'} className='hover:underline'>
-          Click here
-        </Link>
-      </p>
+    <div className='relative w-full overflow-x-auto sm:rounded-lg'>
+      <h2 className='my-5 text-xl font-semibold text-blue'>New transfer</h2>
+      <p className='mb-10 text-sm text-light'>Please complete the following forms to make a transfer</p>
+      <div className='flex h-full w-full items-center justify-between text-white'>
+        <div className='flex h-full w-2/4 items-center justify-center text-center'>
+          <Image src={Transfer} width={1000} height={1000} alt='New transaction' />
+        </div>
+        <TransferForm />
+      </div>
     </div>
   );
 }
--- src/components/dashboard/transfers/transferForm.tsx
+++ src/components/dashboard/transfers/transferForm.tsx
@@ -1,25 +1,69 @@
 'use client';
 
-import { NoAvatar } from '../../../../public/assets';
 import { fetchAccounts } from '@/lib/api/Accounts';
 import { TAccount } from '@/lib/types';
-import Image from 'next/image';
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
+import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
 import { BiTransfer } from 'react-icons/bi';
+import { toast } from 'react-toastify';
 
 export default function TransferForm() {
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
+    reset,
   } = useForm();
+  const router: AppRouterInstance = useRouter();
   const [accounts, setAccounts] = useState<TAccount[]>([]);
+  const balanceAmount = accounts.map((account: TAccount) => {
+    return account.balance.amount;
+  });
 
   const onSubmit = async (data: FieldValues) => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
+    try {
+      const idDebitAccount: string | null = localStorage.getItem('idAccount');
 
-    console.log(data);
+      if (data.amount > balanceAmount) {
+        toast.error('Insufficient balance for the transfer', {
+          theme: 'dark',
+        });
+      }
+
+      const transferData = {
+        idDebitAccount: idDebitAccount,
+        idCreditAccount: data.idCreditAccount || null,
+        reason: data.reason,
+        amount: data.amount,
+        transferDatetime: data.transferDatetime
+          ? new Date(data.transferDatetime).toISOString()
+          : new Date().toISOString(),
+      };
+
+      const response: Response = await fetch('http://localhost:8080/transfer', {
+        method: 'POST',
+        body: JSON.stringify(transferData),
+        headers: {
+          'Content-type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        console.error(`HTTP error! status: ${response.status}`);
+        return;
+      } else {
+        toast.success('Transfer made successfully', {
+          theme: 'dark',
+        });
+      }
+
+      router.push('/dashboard/transaction');
+      reset();
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   useEffect(() => {
@@ -56,20 +100,23 @@ export default function TransferForm() {
         className='mt-1 w-full rounded-md bg-hover p-2 outline-none '
       />
 
-      <label htmlFor='idAccount' className='block text-sm font-medium text-white'>
-        Choose an account to deal with
+      <label htmlFor='idCreditAccount' className='block text-sm font-medium text-white'>
+        Choose an account to deal with <span className='text-xs text-gray-500 '>(You can leave it empty)</span>
       </label>
       <select
-        {...register('idAccount')}
-        id='idAccount'
+        {...register('idCreditAccount')}
+        id='idCreditAccount'
         className='mt-1 w-full rounded-md bg-hover p-2 capitalize outline-none'
       >
+        <option value=''>Select an option</option>
         {accounts
           .filter((account: TAccount) => account.idAccount !== localStorage.getItem('idAccount'))
           .map((account: TAccount) => (
-            <option key={account.idAccount} value={account.idAccount}>
-              {account.firstName} {account.lastName}
-            </option>
+            <>
+              <option key={account.idAccount} value={account.idAccount}>
+                {account.firstName} {account.lastName}
+              </option>
+            </>
           ))}
       </select>
 
