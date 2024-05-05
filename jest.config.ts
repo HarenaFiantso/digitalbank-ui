0002f9a56377cd245d88b18129e7a0f86147c004
HarenaFiantso <fiantsorav@gmail.com>
2024-05-05 15:00:55 +0300

    build: use nx for creating nextjs workspace

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'org',
+  preset: './jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: './coverage/org',
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.[jt]s?(x)',
+    '<rootDir>/src/**/*(*.)@(spec|test).[jt]s?(x)',
+  ],
+};
--- public/assets/index.ts
+++
@@ -1,9 +0,0 @@
-import AccountNotFount from './accountNotFound.webp';
-import AddAccount from './addAccount.gif';
-import MakeTransaction from './makeTransaction.webp';
-import Money from './money.webp';
-import NoTransaction from './noTransaction.png';
-import NoAvatar from './noavatar.png';
-import Transfer from './transfer.png';
-
-export { Money, NoAvatar, AddAccount, AccountNotFount, NoTransaction, MakeTransaction, Transfer };
